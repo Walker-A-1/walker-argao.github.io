27042a5be288e0f6cbcde55e436a14b06ef370f3
Walker-A-1 a <Argao620@gmail.com>
2023-12-30 17:33:26 +0000

    lasdkf;j

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,6 +1,40 @@
 /**
  * Variables:
  * 
+ * 0: Variables are like tentacles pointed at the information we want it to remember. Variables should have a clear and concise name that describes the information they 
+ * refer too. Variables are a useful way to store and reuse data, so that we do not have to remember and retype our information everytime we want to use it.
+ * This also means that we can change the information inside our code without rewriting each piece of data. All we have to do is change what data the variable is change
+ * the data the variable is looking at and we can change what data our code has (where variables are inserted).
+ * 
+ * 1: Declaration and Assinment:
+ * Declaration means creating a variable. Communicating to the computer "I want to make a variable". Using the keyword `var`, `let`, or `const`, followed by the name 
+ * of your variable.
+ * 
+ * Assinment is when you tell your variable what information to hold onto. This can be done when you create the variable, later, or not at all.
+ *  
+ * 2: Var, Let, Const: 
+ * Var, let, and const are the keywords you need to make a variable. Var is the oldest and most problamatic, k
+ * 
+ * 3: Hoisting:
+ */
+
+// 1. Declaration 
+//Here I am telling javascript that I want to make a variable called variable. 
+//Note: the value is undefined because I did not give this tentacle anything to hold onto.
+var variable;
+
+console.log(variable); // this will return `undefined`
+
+//1. Assinment
+//Here I am declaring a new variable `amString` and telling it to hold onto the value of `hiiiii`.
+var amString = 'hiiiii';
+
+console.log(amString); //this will return our value `hiiiii` because that is what we told the variable to hold on to.
+
+
+/** leave this here so I don't forget
+ * Variables:
+ * 
  * 0: no more than 5 sentences needed for introduction.
  * 
  * 1: then describe the sections listed on the slide
