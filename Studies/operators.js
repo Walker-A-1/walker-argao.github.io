3df65f3a4e42dab8a5269a445d6016653b1f0625
Walker-A-1 a <Argao620@gmail.com>
2024-01-01 00:23:10 +0000

    just started on operators studies project

---
+++ Studies/operators.js
@@ -0,0 +1,25 @@
+/**
+ * 0. Intro:
+ * 
+ * 1. Assignment operators:
+ * 
+ * 2. Arithmetic operators:
+ * 
+ * 3. Comparison operators:
+ * 
+ * 4. Logical operators:
+ * 
+ * 5. Unary operators (!, typeOf, -):
+ * 
+ * 6. Ternary operator (a ? b : c):
+ * 
+ * 
+ */
+
+//1. Assignment Operators
+//explain example code
+//example code of =, +=, -=, *=, /=
+
+//2. Arithmetic operators
+//explain example code
+//example code of +, -, ++, --
