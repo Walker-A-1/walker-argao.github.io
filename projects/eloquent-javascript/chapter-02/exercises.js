ce6adcf6fc7896b45f6d1f73a82861ca15fd9e66
Walker-A-1 a <Argao620@gmail.com>
2024-01-03 04:38:13 +0000

    finished the chessboard shitter!

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -42,6 +42,8 @@ function fizzBuzz() {
  # # #
  */
 function drawChessboard(n) {
+  //make a result string
+  let result = '';
   //make a storage function called `odd`
     //assign it a for loop that will draw the first chess board
     //for loop. start num at 1, end after n, step 1
@@ -50,18 +52,46 @@ function drawChessboard(n) {
       //check if num is even
       if (num % 2 === 0) {     
         //if num is even then push a space" " to the storage function
-        odd += ' ';
+        odd += '#';
       } else {
         //if num is odd then push a hashtag to the storage funciton
-        odd += '#';
+        odd += ' ';
       }
     }
-  return odd;
 
   //make a storage string called `even`
-
-  //make a result string
-    
+  let even = '';
+    for (let num = 1; num <= n; num++) {
+      //check if num is even
+      if (num % 2 === 0) {     
+        //if num is even then push a space" " to the storage function
+        even += ' ';
+      } else {
+        //if num is odd then push a hashtag to the storage funciton
+        even += '#';
+      }
+    }
+    //make a for loop to print the layers you need
+    //start num = 1; stop after n; step 1
+    for(let num = 1; num <= n; num++) {
+      //check if this is the first time we have done this
+      if(num === 1) {
+        //if yes assign the result variable to an odd layer without a newline
+        result = odd;
+      } else {
+      //if this is not the first time we have done this then
+        //check if num is even
+        if(num % 2 === 0) {
+           //if yes then concatinate an even layer with a newline before it to the result String
+          result += `\n${even}`;
+        } else {
+           //else concatinate an odd layer with a newline before it to the result String
+          result += `\n${odd}`;
+        }
+      }
+    }
+  //console.log the result String
+  console.log(result + `\n`);
 
 }
 console.log(`return statement of 4 ${drawChessboard(4)}`);
